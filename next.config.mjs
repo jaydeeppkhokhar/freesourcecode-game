6036df15cc034b05a6e23c884a9c9fa0a9dac093
Jaydeep Khokhar <jaydeeppkhokhar@gmail.com>
2024-11-21 19:59:55 +0530

    coolify changes added

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // experimental: {
-  //   missingSuspenseWithCSRBailout: false,
-  // },
   eslint: {
     ignoreDuringBuilds: true,
-  }
+  },
+  experimental: {
+    appDir: true, // Enables the app directory structure
+    serverActions: true, // Fixes issues with server-side rendering and actions
+  },
+  reactStrictMode: true, // Ensures better checks during development
 };
 
 export default nextConfig;
